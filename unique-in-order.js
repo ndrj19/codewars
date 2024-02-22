fa4373ba3de5f61942cb52941915c1444dc8c581
Andrej Hagenmüller <andrej.hagenmueller@gmail.com>
2024-02-22 15:38:05 +0000

    add problem

---
+++ unique-in-order.js
@@ -0,0 +1,13 @@
+const uniqueInOrder = (iterable) => {
+  return [...iterable].filter((e, i) => e !== iterable[i - 1]);
+};
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
+
+/*
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
