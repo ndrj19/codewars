0627ff17be531dbb71110a18508b3d41e4b05ad0
Andrej Hagenmüller <andrej.hagenmueller@gmail.com>
2024-02-24 15:54:24 +0000

    add problem

--- fibmemo.js
+++ fibmemo.js
@@ -1,7 +1,7 @@
 const fib = (n, memo = {}) => {
   if (n in memo) return memo[n];
   if (n <= 2) return 1;
-  memo[n] = fib(n - 1) + fib(n - 2);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
   return memo[n];
 };
 
@@ -9,3 +9,4 @@ console.log(fib(5));
 console.log(fib(6));
 console.log(fib(8));
 console.log(fib(20));
+console.log(fib(50));
